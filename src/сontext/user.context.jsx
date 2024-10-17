43eff7e7cb9fc6f612c0682337736e3c5d66d172
Nikita Kaberov <enagtim@mail.ru>
2024-10-17 10:19:30 +0300

    Профиль добавляется в массив профилей, а при выходе изменяется cтатус логина

--- src/сontext/user.context.jsx
+++ src/сontext/user.context.jsx
@@ -6,32 +6,54 @@ export const UserContext = createContext();
 export const UserContextProvider = ({ children }) => {
   const [username, setUserName] = useState("");
   const [isLogin, setIsLogin] = useState(false);
+  const [profiles, setProfiles] = useState([]);
 
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem("profiles"));
-    if (data) {
-      setUserName(data.name);
-      setIsLogin(data.isLogin);
+    if (Array.isArray(data)) {
+      setProfiles(data);
+      const loggedUser = data.find((profile) => profile.isLogin);
+      if (loggedUser) {
+        setUserName(loggedUser.name);
+        setIsLogin(true);
+      }
+    } else {
+      setProfiles([]);
     }
   }, []);
 
   const login = (name) => {
+    const data = JSON.parse(localStorage.getItem("profiles")) || [];
+    const existingProfile = data.find((profile) => profile.name === name);
+
+    if (existingProfile) {
+      existingProfile.isLogin = true;
+    } else {
+      data.push({ name, isLogin: true });
+    }
+
+    setProfiles(data);
+    localStorage.setItem("profiles", JSON.stringify(data));
     setUserName(name);
     setIsLogin(true);
-    const user = {
-      name: name,
-      isLogin: true,
-    };
-    localStorage.setItem("profiles", JSON.stringify(user));
   };
 
   const logout = () => {
+    const data = JSON.parse(localStorage.getItem("profiles")) || [];
+    const updateProfile = data.map((profile) => {
+      if (profile.name === username) {
+        return { ...profile, isLogin: false };
+      }
+      return profile;
+    });
+    localStorage.setItem("profiles", JSON.stringify(updateProfile));
     setUserName("");
     setIsLogin(false);
-    localStorage.removeItem("profiles");
   };
   return (
-    <UserContext.Provider value={{ username, isLogin, login, logout }}>
+    <UserContext.Provider
+      value={{ profiles, username, isLogin, login, logout }}
+    >
       {children}
     </UserContext.Provider>
   );
