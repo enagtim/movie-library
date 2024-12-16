e449df0383c9dbfedfc96488bd01c9a87682efb2
Nikita <enagtim@mail.ru>
2024-12-16 22:24:43 +0300

    Убран конфиг из Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost',
-      port: 3000
-    },
-    open: true,
     watch: {
       usePolling: true
     }
